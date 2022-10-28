81dfb56037217c2b054d612b8bc9cc543eec1584
Craig Tomkow <ctomkow@gmail.com>
2022-10-27 20:56:23 -0700

    cleanup

--- gluetube/pipeline.py
+++ gluetube/pipeline.py
@@ -13,26 +13,12 @@ import os
 
 class Pipeline(ABC):
 
-    def __init__(self, *variable_groups) -> None:
-
-        # status is the outside view of the pipeline (e.g. from command.py point of view)
-        # self.status = ['running', 'crashed', 'completed']
-
-        # stage is inside view of pipeline (e.g. reporting from within via decorators)
-        # self.stage = ['1', ... '9']
-
-        # if 'running', the stage is ['1', ... '9']
-        # if 'crashed', the last stage the pipeline was in will be shown
-        # if 'completed', the last state of the pipeline was in will be shown (e.g. '4')
-
-        # TODO: set pipeline stage='initializing', an sqlite3 call
-
-        # TODO: check to see if PPID is still alive, if not, then exit (to stop the cron)
+    def __init__(self, keyvalue_groups: list) -> None:
 
         db_store = Store('store.db')
 
         # injecting stored key:values as environment variables into the running pipeline process context
-        for group in variable_groups:
+        for group in keyvalue_groups:
             key_values = db_store.all_key_values(group)
             for kv in key_values:
                 os.environ[f"{group}_{kv[0]}"] = kv[1]
